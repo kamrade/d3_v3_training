80ea651fdf6ffcecb565d26b61b395e76427436a
Denis Michailov <kamrad.reg@gmail.com>
2016-07-18 21:59:24 +1000

    Basic Shapes

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,8 @@ var $c_01 = '#21b1e0',
 
 console.log( "D3 version: " + d3.version );
 
-var width = 500;
-var height = 500;
+var width = 400;
+var height = 200;
 
 d3.select("h1.title")
 	.text("D3 JS testing");
@@ -15,3 +15,25 @@ d3.select(".head-text")
 	.style("color", $c_01)
 	.text("Practice and learnng 3-rd version of d3");
 
+var canvas = d3.select(".work")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var circle = canvas.append("circle")
+	.attr("cx", width/2)
+	.attr("cy", height/2)
+	.attr("r", (width+height)/2/4)
+	.attr("fill", $c_02);
+
+var rect = canvas.append("rect")
+	.attr("width", 100)
+	.attr("height", 50);
+
+var line = canvas.append("line")
+	.attr("x1", 0)
+	.attr("y1", 100)
+	.attr("x2", 400)
+	.attr("y2", 400)
+	.attr("stroke", $c_02)
+	.attr("stroke-width", 5);
