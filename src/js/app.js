9e6be9728d0f526ad5f07c175cec219003c4ef61
Denis Michailov <kamrad.reg@gmail.com>
2016-07-18 21:33:11 +1000

    Inline styling

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,14 @@ var $c_01 = '#21b1e0',
 
 console.log( "D3 version: " + d3.version );
 
+var width = 500;
+var height = 500;
 
 d3.select("h1.title")
 	.text("D3 JS testing");
 
 d3.select(".head-text")
 	.append("p")
+	.style("color", $c_01)
 	.text("Practice and learnng 3-rd version of d3");
+
