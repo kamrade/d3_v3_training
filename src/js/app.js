a1e16e18846d89b762a62b0c04a113da6270f116
Denis Michailov <denis.michailov@gmail.com>
2016-07-19 17:49:44 +1000

    Transitions

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
-// DOM elements < data elements (enter)
-// DOM elements > data elements (exit)
-// DOM elements = data elements (update)
-
 var $c_01 = '#21b1e0',
 	$c_02 = '#1abc9c',
 	$c_03 = '#1d7e6b',
@@ -14,31 +10,23 @@ var data = [10]
 	width = 500,
 	height = 500;
 
-
 var canvas = d3.select(".work")
 	.append("svg")
 	.attr("width", width)
 	.attr("height", height);
 
-var circle01 = canvas.append("circle")
-	.attr("cx", 40)
-	.attr("cy", 40)
-	.attr("r", 20);
+var circle = canvas.append("circle")
+	.attr("cx", 100)
+	.attr("cy", 100)
+	.attr("r", 25)
+	.style("transition", "ease .3s all");
 
-var circle02 = canvas.append("circle")
-	.attr("cx", 140)
-	.attr("cy", 140)
-	.attr("r", 20);
-
-var circles = canvas.selectAll("circle")
-	.data(data)
-	.attr("fill", $c_03)
-	.exit()
-		.attr("fill", $c_01);
-	// .attr("fill", $c_02) // update selection
-	// .enter()
-	// 	.append("circle") // enter selection
-	// 	.attr("cx", width/2)
-	// 	.attr("cy", height/2)
-	// 	.attr("fill", $c_01)
-	// 	.attr("r", (height+width)/20);
+circle.transition()
+	.duration(1500)
+	.delay(2000)
+	.attr("cx", 400)
+	.each("start", function() { d3.select(this).attr("fill", "blue") })
+	.each("end", function() { d3.select(this).attr("fill", "red") })
+	.transition()
+	.attr("cy", 300)
+	.attr("cx", 100);
