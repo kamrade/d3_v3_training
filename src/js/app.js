5211d46471402c3d7ced79ddb144de509771d085
Denis Michailov <denis.michailov@gmail.com>
2016-07-19 14:31:40 +1000

    Groups and axes

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,20 @@ var $c_01 = '#21b1e0',
 
 console.log( "D3 version: " + d3.version );
 
-var width = 500;
+// options
+var width = 600;
 var height = 500;
-var barHeight = 20;
 
-var dataArray = [20, 40, 314, 200, 100, 280, 12, 19, 49];
+var margins = 20;
+var workWidth = width - (margins * 2);
+var barHeight = 20;
+var barDistance = 2;
+
+
+
+// data
+var dataArray = [20, 40, 314, 200, 100, 280, 12, 19, 49, 80, 60, 212];
+var ticks = 5;
 
 d3.select("h1.title")
 	.text("D3 JS testing");
@@ -30,10 +39,16 @@ var colorScale = d3.scale.linear()
 	.domain([0, d3.max(dataArray)])
 	.range([ $c_min, $c_max ]);
 
+var axis = d3.svg.axis()
+	.ticks(ticks)
+	.scale(widthScale);
+
 var canvas = d3.select(".work")
 	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
+	.attr("width", width + 2*margins)
+	.attr("height", height)
+	.append("g")
+	.attr("transform", "translate(" + margins + ", " + margins + ")");
 
 
 var bars = canvas.selectAll("rect")
@@ -43,7 +58,12 @@ var bars = canvas.selectAll("rect")
 		.attr("width", function(d) { return widthScale(d); })
 		.attr("height", barHeight)
 		.attr("fill", function(d) { return colorScale(d); })
-		.attr("y", function(d, i) { return (barHeight + 2) * i });
+		.attr("y", function(d, i) { return (barHeight + barDistance) * i });
+
+canvas.append("g")
+	.attr("transform", "translate(0, " + (dataArray.length*barHeight + dataArray.length*barDistance) + ")")
+	.attr("class", "axis-bottom")
+	.call(axis);
 
 
 
