e7f34741261738d93a13ba26797a82e319649459
Denis Michailov <denis.michailov@gmail.com>
2016-07-19 17:26:46 +1000

    Enter Update Exit

--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,44 @@
+// DOM elements < data elements (enter)
+// DOM elements > data elements (exit)
+// DOM elements = data elements (update)
+
 var $c_01 = '#21b1e0',
 	$c_02 = '#1abc9c',
 	$c_03 = '#1d7e6b',
 	$c_min = '#0c5636',
 	$c_max = '#27c681';
 
-
-
 console.log( "D3 version: " + d3.version );
 
-// options
-var width = 600;
-var height = 500;
+var data = [10]
+	width = 500,
+	height = 500;
 
-var margins = 20;
-var workWidth = width - (margins * 2);
-var barHeight = 20;
-var barDistance = 2;
-
-
-
-// data
-var dataArray = [20, 40, 314, 200, 100, 280, 12, 19, 49, 80, 60, 212];
-var ticks = 5;
-
-d3.select("h1.title")
-	.text("D3 JS testing");
-
-d3.select(".head-text")
-	.append("p")
-	.style("color", $c_01)
-	.text("Practice and learnng 3-rd version of d3");
-
-var widthScale = d3.scale.linear()
-	.domain([0, d3.max(dataArray)])
-	.range([0, width]);
-
-var colorScale = d3.scale.linear()
-	.domain([0, d3.max(dataArray)])
-	.range([ $c_min, $c_max ]);
-
-var axis = d3.svg.axis()
-	.ticks(ticks)
-	.scale(widthScale);
 
 var canvas = d3.select(".work")
 	.append("svg")
-	.attr("width", width + 2*margins)
-	.attr("height", height)
-	.append("g")
-	.attr("transform", "translate(" + margins + ", " + margins + ")");
+	.attr("width", width)
+	.attr("height", height);
 
+var circle01 = canvas.append("circle")
+	.attr("cx", 40)
+	.attr("cy", 40)
+	.attr("r", 20);
 
-var bars = canvas.selectAll("rect")
-	.data(dataArray)
-	.enter()
-		.append("rect")
-		.attr("width", function(d) { return widthScale(d); })
-		.attr("height", barHeight)
-		.attr("fill", function(d) { return colorScale(d); })
-		.attr("y", function(d, i) { return (barHeight + barDistance) * i });
+var circle02 = canvas.append("circle")
+	.attr("cx", 140)
+	.attr("cy", 140)
+	.attr("r", 20);
 
-canvas.append("g")
-	.attr("transform", "translate(0, " + (dataArray.length*barHeight + dataArray.length*barDistance) + ")")
-	.attr("class", "axis-bottom")
-	.call(axis);
-
-
-
+var circles = canvas.selectAll("circle")
+	.data(data)
+	.attr("fill", $c_03)
+	.exit()
+		.attr("fill", $c_01);
+	// .attr("fill", $c_02) // update selection
+	// .enter()
+	// 	.append("circle") // enter selection
+	// 	.attr("cx", width/2)
+	// 	.attr("cy", height/2)
+	// 	.attr("fill", $c_01)
+	// 	.attr("r", (height+width)/20);
