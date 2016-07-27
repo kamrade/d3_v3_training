1f088130938a8c0defa4000de8b216d31baa68cb
Denis Michailov <denis.michailov@gmail.com>
2016-07-27 10:21:09 +1000

    Pie Chart

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,28 @@ var $c_01 = '#21b1e0',
 	$c_min = '#0c5636',
 	$c_max = '#27c681';
 
+var $c_white = '#fff',
+	$c_bg_light = '#f5f8fd',
+	$c_border_light = '#d8e1ea',
+	$c_text_light_01 = '#778796',
+	$c_bg_dark_selected = '#555e75',
+	$c_bg_dark_main = '#383f52';
+
+var colorScale = d3.scale.ordinal()
+	.range([ 
+		$c_bg_light,
+		$c_border_light,
+		$c_text_light_01,
+		$c_bg_dark_selected,
+		$c_bg_dark_main
+	]);
+
 console.log( "D3 version: " + d3.version );
 
 var width = 500,
-	height = 500;
+	height = 500,
+	r = 150,
+	data = [10, 50, 80, 28, 450];
 
 
 var canvas = d3.select(".work").append("svg")
@@ -15,19 +33,38 @@ var canvas = d3.select(".work").append("svg")
 	.attr("width", width);
 
 var group = canvas.append("g")
-	.attr( "transform" , "translate(100, 100)" );
-
-var r = 100;
-var p = Math.PI * 2;
+	.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
 
 var arc = d3.svg.arc()
-	.innerRadius(r - 20)
-	.outerRadius(r)
-	.startAngle(0)
-	.endAngle(p - 1);
+	.innerRadius(r/4)
+	.outerRadius(r);
 
-group.append("path")
-	.attr("d", arc);
+var pie = d3.layout.pie()
+	.value(function(d) {
+		return d;
+	});
+
+var arcs = group.selectAll(".arc")
+	.data(pie(data))
+	.enter()
+		.append("g")
+		.attr("class", "arc");
+
+arcs.append("path")
+	.attr("d", arc)
+	.attr("fill", function(d) {
+		return colorScale(d.data);
+	});
+
+arcs.append("text")
+	.attr("transform", function(d) {
+		return "translate(" + arc.centroid(d) + ")" ;
+	})
+	.attr("text-anchor", "middle")
+	.attr("font-size", ".5em")
+	.text(function(d) {
+		return d.data;
+	});
 
 
 
